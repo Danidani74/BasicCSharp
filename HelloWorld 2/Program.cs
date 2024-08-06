dc328530d62afd535fa80373255b7f3d2b0621e3
USER <USER@DESKTOP-TPJ8P65>
2024-08-06 11:26:10 +0700

    Add project files.

---
+++ HelloWorld 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama;
+            Console.WriteLine("Masukan nama : ");
+            nama = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hallo " + nama + " Selamat datang di C#!");
+        }
+    }
+}
